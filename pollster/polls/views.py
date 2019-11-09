52fa7de59bdc4dbac44ce57eb98e209282b46e02
haydane <haydane9@gmail.com>
2019-11-09 13:11:37 +0700

    first init

---
+++ pollster/polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,Http404,get_object_or_404
+from .models import Question,Choice
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request,'polls/index.html', context)
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does not Exist")
+    return render(request,'polls/results.html',{'question': question})
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results',{'question',question})
